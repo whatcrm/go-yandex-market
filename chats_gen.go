03bfef70467ff6b166c92f29a38d0e9a7a68a2ef
wileytork <a.petrova@whatcrm.net>
2026-09-07 14:42:20 +0300

    fix

--- chats_gen.go
+++ chats_gen.go
@@ -4,7 +4,9 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"io"
+	"mime/multipart"
 	"net/http"
 
 	"github.com/whatcrm/go-yandex-market/models"
@@ -149,6 +151,10 @@ func (c *Client) GetChats(ctx context.Context, businessId int64, params *models.
 }
 
 func (c *Client) SendFileToChat(ctx context.Context, businessId int64, params *models.SendFileToChatParams, body *models.SendFileToChatRequest) (*models.EmptyApiResponse, error) {
+	if body == nil {
+		return nil, errors.New("body is required")
+	}
+
 	requestURL := c.BaseURL + utils.BuildPath(chats.SendFileToChatEndpoint, utils.PathInt("businessId", businessId))
 	var err error
 
@@ -157,26 +163,39 @@ func (c *Client) SendFileToChat(ctx context.Context, businessId int64, params *m
 		return nil, err
 	}
 
-	var reqBody io.Reader
-
-	var jsonBody []byte
-	jsonBody, err = json.Marshal(body)
+	data, err := body.File.Bytes()
 	if err != nil {
 		return nil, err
 	}
-	reqBody = bytes.NewReader(jsonBody)
+	filename := body.File.Filename()
+	if filename == "" {
+		filename = "file"
+	}
 
-	req, err := http.NewRequestWithContext(ctx, "POST", requestURL, reqBody)
+	var buf bytes.Buffer
+	writer := multipart.NewWriter(&buf)
+	part, err := writer.CreateFormFile("file", filename)
 	if err != nil {
 		return nil, err
 	}
+	if _, err = part.Write(data); err != nil {
+		return nil, err
+	}
+	if err = writer.Close(); err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequestWithContext(ctx, "POST", requestURL, &buf)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", writer.FormDataContentType())
 
 	var response models.EmptyApiResponse
 	if err = c.Send(req, &response); err != nil {
 		return nil, err
 	}
 	return &response, nil
-
 }
 
 func (c *Client) SendMessageToChat(ctx context.Context, businessId int64, params *models.SendMessageToChatParams, body *models.SendMessageToChatRequest) (*models.EmptyApiResponse, error) {
